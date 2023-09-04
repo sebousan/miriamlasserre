9ebcf2c45999132d06d57d06995bbaea1163b912
sebousan <sebousan@gmail.com>
2023-09-04 16:37:00 +0200

    update CMS and theme

--- static/admin/app/settings/available-collections.js
+++ static/admin/app/settings/available-collections.js
@@ -1,5 +1,9 @@
-export const availableCollections = [
-    'services',
-    'ebooks',
-    'publications'
-];
+import services from '../content/services.js';
+import ebooks from '../content/ebooks.js';
+import publications from '../content/publications.js';
+
+export default [
+    services,
+    ebooks,
+    publications
+]
