84e105e09aa6450ab3ff8505eb93f5ada85f2f39
sebousan <sebousan@gmail.com>
2024-11-05 14:46:06 +0100

    improve js

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+// Utils
+import './utils/global';
+
+// Vendors
+import './vendors/bootstrap';
+
+// Blocks
+// import './blocks/chart';
+// import './blocks/instagram';
+
+// Components
+// import './components/animation';
+import './components/carousel';
+// import './components/gallery';
+// import './components/gauge';
+// import './components/map';
+import './components/menu';
+import './components/toc';
+// import './components/tooltip';
+// import './components/video';
